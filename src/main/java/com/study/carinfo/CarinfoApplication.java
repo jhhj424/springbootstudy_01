37c1443190c8e460f12f77aafea8c64ed04bf4f8
jiho shin <jhhj424@naver.com>
2021-05-05 15:05:07 +0900

    Initial commit

---
+++ src/main/java/com/study/carinfo/CarinfoApplication.java
@@ -0,0 +1,13 @@
+package com.study.carinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarinfoApplication.class, args);
+	}
+
+}
