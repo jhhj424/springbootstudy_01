c3f0e77a04eef3f8c9b9941b573efe8417e0dd95
jiho shin <jhhj424@naver.com>
2021-05-12 00:02:49 +0900

    feat: Add API (/companyList)

--- src/main/java/com/study/carinfo/controller/BasicController.java
+++ src/main/java/com/study/carinfo/controller/BasicController.java
@@ -1,8 +1,12 @@
 package com.study.carinfo.controller;
 
+import com.study.carinfo.domain.Company;
 import com.study.carinfo.service.CompanyInputDTO;
 import com.study.carinfo.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -30,5 +34,12 @@ public class BasicController {
         companyService.saveCompanyInputDto(companyInputDTO);
 
         return "index";
+    }
+
+    @RequestMapping("/companyList")
+    public String companyList(@PageableDefault Pageable pageable, Model model) {
+        Page<Company> companies = companyService.getCompanyPage(pageable);
+        model.addAttribute("companyList", companies);
+        return "companyList";
     }
 }
