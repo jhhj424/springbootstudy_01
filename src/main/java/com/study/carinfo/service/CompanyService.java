d9c271c7d167b1d3d6b46af714947980d2c78513
jiho shin <jhhj424@naver.com>
2021-05-13 21:00:34 +0900

    feat: 회사 목록 조회 (페이징) 기능 추가

--- src/main/java/com/study/carinfo/service/CompanyService.java
+++ src/main/java/com/study/carinfo/service/CompanyService.java
@@ -3,6 +3,10 @@ package com.study.carinfo.service;
 import com.study.carinfo.domain.Company;
 import com.study.carinfo.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,5 +37,12 @@ public class CompanyService {
 
     public void saveAll(List<Company> companies) {
         companyRepository.saveAll(companies);
+    }
+
+    public Page<Company> getCompanyPage(Pageable pageable) {
+        int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber() - 1);
+        pageable = PageRequest.of(page, 10, Sort.by(Sort.Direction.DESC, "id"));
+
+        return companyRepository.findAll(pageable);
     }
 }
